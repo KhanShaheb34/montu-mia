702e110d74576ffdb7d9561790023dc189923546
KhanShaheb34 <shakirulhkhan@gmail.com>
2026-01-30 00:16:57 +0600

    feat: add subscribe button on hook page

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,13 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Mermaid } from "./components/mdx/mermaid";
+import { SubscribeModal } from "@/components/subscribe-modal";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...(defaultMdxComponents as MDXComponents),
     Mermaid,
+    SubscribeModal,
     ...components,
   };
 }
